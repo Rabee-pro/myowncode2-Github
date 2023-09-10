5c9371eaf292126c8652eb69580885c1167d2825
Rabeena.R <rabeenalearning123@gmail.com>
2023-09-10 10:22:14 +0530

    first commit

---
+++ try3.js
@@ -0,0 +1,13 @@
+// Side Menu toggle
+let myMenu = document.querySelector(".hamburgerMenu");
+let sideMenu = document.querySelector(".sideMenu");
+myMenu.addEventListener('click',()=>{
+    sideMenu.classList.toggle('sideMenu')
+})
+
+//  Pages Toggle
+//  let page = document.querySelector('.page_manu');
+//  let pageBabys = document.querySelector('.Authentic');
+//  page.addEventListener('click',()=>{
+//      pageBabys.classList.toggle('Authentic')
+//  })
